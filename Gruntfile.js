866dc57acc92f7cf0974591bd9237d3abd1a546d
Zeno Rocha <zeno.rocha@liferay.com>
2014-04-06 21:55:08 -0300

    Create Hello World example based on Polymer

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        connect: {
+            demo: {
+                options: {
+                    open: true,
+                    keepalive: true
+                }
+            }
+        },
+        replace: {
+            example: {
+                src: ['src/*'],
+                dest: 'dist/',
+                replacements: [{
+                    from: 'bower_components',
+                    to: '..'
+                }]
+            }
+        },
+        bump: {
+            options: {
+                commit: true,
+                commitFiles: ['bower.json', 'package.json'],
+                commitMessage: 'Release v%VERSION%',
+                createTag: true,
+                files: ['bower.json', 'package.json'],
+                push: true,
+                pushTo: 'origin gh-pages',
+                tagMessage: '',
+                tagName: 'v%VERSION%'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-text-replace');
+
+    grunt.registerTask('default', ['connect']);
+    grunt.registerTask('build', ['replace']);
+
+};
